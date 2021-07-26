86b9f659b399b7a044e2477952d9b4a168740683
Nicolas <Nicolas@DESKTOP-9132GIN>
2021-07-25 21:29:06 -0300

    second commit v2 de teclado

--- teclado_v2.0.py
+++ teclado_v2.0.py
@@ -1,11 +1,11 @@
 import string as s
 
-letras = [' ', ('a', 'b', 'c'), ('d', 'e', 'f'),('g', 'h', 'i'), ('j', 'k', 'l'), ('m', 'n', 'o'), ('p', 'q', 'r', 's'), ('t', 'u', 'v'), ('w', 'x', 'y', 'z') ]
 
 
 
 
 def mensaje_valido(mensaje):
+    """Funcion para ingresar solo caracteres del alfabeto en minúsculas"""
 
     no_validos = s.digits+s.punctuation+s.ascii_uppercase
     caracter_inv = "Ha ingresado un caracter no válido, por favor ingrese solo letras minúsculas."
@@ -17,13 +17,32 @@ def mensaje_valido(mensaje):
             break   
         else:
             return list(mensaje)
-def mapear_caracteres(lista):
-    pass
+
+
+def mapear_caracteres(mensaje):
+    letras = [' ', ('a', 'b', 'c'), ('d', 'e', 'f'),('g', 'h', 'i'), ('j', 'k', 'l'), ('m', 'n', 'o'), ('p', 'q', 'r', 's'), ('t', 'u', 'v'), ('w', 'x', 'y', 'z') ]
+
+    #if [(c, cs)  for c in lista for cs in letras if c == cs]:
+    #    yield print(True)
+    for letra in mensaje:
+        print("letra en mensaje: ",str(letra))
+        for  elemento in range(0, len(letras)):
+            print(letras[elemento])
+            for caracter in range(0, len(elemento)):
+                if letra in letras:
+                    print("encuentra")
+                    return letras[elemento][caracter]
+                #else:
+                    print("no coincidencia")
 
 
 print("\n\nTECLADO NUMERICO\n")
-    
+resultado =[]
 while(True):
     msj=input("Ingrese un mensaje para codificar: ")
     resultado = mensaje_valido(msj)
     print(resultado)
+    try:
+        print(mapear_caracteres(resultado))
+    except:
+            continue
