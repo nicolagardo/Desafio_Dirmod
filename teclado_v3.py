7fefacf78ef1e387bc8557267b067db9bd00dc5d
Nicolas <Nicolas@DESKTOP-9132GIN>
2021-08-03 23:29:08 -0300

    mejora

--- teclado_v3.py
+++ teclado_v3.py
@@ -1,10 +1,26 @@
-alfabeto ={'0' : ' ', '2' : ['a', 'b', 'c'], '3' : ['d', 'e', 'f'], '4' : ['g', 'h', 'i'], '5' : ['j', 'k', 'l'],'6' : ['m', 'n', 'o'], '7' : ['p', 'q', 'r', 's'], '8' : ['t', 'u', 'v'],'9' : ['w', 'x', 'y', 'z']}
+from funciones import mensaje_valido as mv
+import string as s
+
+alfabeto ={'0' : ' ', '2' : ['a', 'b', 'c'], '3' : ['d', 'e', 'f'], '4' : ['g', 'h', 'i'], '5' : ['j', 'k', 'l'],
+'6' : ['m', 'n', 'o'], '7' : ['p', 'q', 'r', 's'], '8' : ['t', 'u', 'v'],'9' : ['w', 'x', 'y', 'z']}
 
 def obtener_clave(val):
     for clave, valor, in alfabeto.items():
         for elemento in range(len(valor)):
          if val == valor[elemento]:
              return clave #*(value.index(elemento) +1) 
-lista =['h', 'o', 'l', 'a']  
-for letra in lista:
+
+
+
+
+
+print("\n\nTECLADO NUMERICO v3\n")
+resultado =[]
+while(True):
+    msj=input("Ingrese un mensaje para codificar: ")
+    resultado = mv(msj)
+    if resultado :
+        break
+    print(resultado)
+for letra in resultado:
     print(obtener_clave(letra))
